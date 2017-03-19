a5c1756c0c13c38f53d8fe58bbf957bb84debb3a
smruthimg <sg1626@rit.edu>
2017-03-19 02:20:47 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function main(){
+var $skillset=$('.subheading');
+$skillset.hide();
+$('.skillset').fadeIn(1000);
+$('.projects').hide();
+$('.subheading-button').on('click',function(){
+$(this).next().slideToggle(1000);
+$(this).toggleClass('active');});
+$('.subheading').on('click',function(){
+$(this).slideToggle(1000);
+$(this).toggleClass('active');
+});
+
+}
+
+$(document).ready(main);
